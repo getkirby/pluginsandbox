40a750ffee19d75cbcc40210dcfef1952da76f56
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-04 16:26:40 +0200

    Install the Komments Plugin

---
+++ plugins/komments/site/config/config.php
@@ -0,0 +1,6 @@
+<?php 
+
+return [
+	'mauricerenck.komments.storage.type'       => 'sqlite',
+	'mauricerenck.komments.storage.sqlitePath' => __DIR__ . '/../db',
+];
---
+++ plugins/komments/site/templates/default.php
@@ -0,0 +1,7 @@
+<head>
+	<?= css('/media/plugins/mauricerenck/komments/komments.css', ['defer' => true]) ?>
+</head>
+
+<h1>Komments</h1>
+
+<?php snippet('komments/kommentform'); ?>
