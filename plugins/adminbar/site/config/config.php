ec99e0da465afbefbeca802b8a3c3cd085d48fa5
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-19 21:58:44 +0200

    Install Adminbar plugin

---
+++ plugins/adminbar/site/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'ready' => function ($kirby) {
+        return [
+            'pechente.kirby-admin-bar' => [
+                'active' => $kirby->user() !== null
+            ]
+        ];
+    },
+];
---
+++ plugins/adminbar/site/templates/default.php
@@ -0,0 +1,16 @@
+<html>
+
+    <head>
+        <title><?= $page->title() ?></title>
+        <style>
+            body {
+                padding: 6rem;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1><?= $page->title() ?></h1>
+    </body>
+</html>
+
