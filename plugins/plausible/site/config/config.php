ec0483c1bb4e3048586c9ad4e048bec3aa6ae64d
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-19 22:27:48 +0200

    Install Plausible plugin

---
+++ plugins/plausible/site/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'panel' => [
+		'frameAncestors' => ['https://plausible.io'],
+	],
+	'floriankarsten.plausible' => [
+		'sharedLink' => 'https://plausible.io/plausible.io',
+	]
+];
